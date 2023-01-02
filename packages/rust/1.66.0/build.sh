f944724f3be5d25427120452203d6cf6d05691e8
ooliver1 <oliverwilkes2006@icloud.com>
2023-01-02 19:47:33 +0000

    fix: why does js require 1 gigabyte to run

--- packages/rust/1.66.0/build.sh
+++ packages/rust/1.66.0/build.sh
@@ -3,3 +3,4 @@
 curl -OL "https://static.rust-lang.org/dist/rust-1.66.0-x86_64-unknown-linux-gnu.tar.gz"
 tar xzvf rust-1.66.0-x86_64-unknown-linux-gnu.tar.gz
 rm rust-1.66.0-x86_64-unknown-linux-gnu.tar.gz
+rm -r rust-1.66.0-x86_64-unknown-linux-gnu/rust-docs

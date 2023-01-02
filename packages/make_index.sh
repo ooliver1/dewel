d6e9b767a4bc3f67501ba32018342ec60ed20a51
ooliver1 <oliverwilkes2006@icloud.com>
2023-01-02 01:37:34 +0000

    sus amogus

--- packages/make_index.sh
+++ packages/make_index.sh
@@ -3,17 +3,15 @@
 export PLATFORM=docker-debian
 make build-all
 
-NL=$'\n'
-
+rm archives/index.csv
 find . -name "*.pkg.tar.gz" | xargs cp -n -t ./archives
-index=""
+
 for file in ./archives/*.pkg.tar.gz; do
     base_name=$(basename $file)
     language=$(cut -d '-' -f 1 <<< $base_name)
     version=$(cut -d '-' -f 2 <<< $base_name | awk -F'.pkg.tar.gz' '{print $1}')
     sha=$(sha256sum $base_name | cut -d ' ' -f 1)
-    url="https://github.com/ooliver1/dewel/raw/master/packages/archives/$base_name"
-    index+="$NL$language,$version,$sha,$url"
+    url="https://github.com/ooliver1/dewel/releases/download/pkgs/$base_name"
+    echo "$language,$version,$sha,$url" >> archives/index.csv
 done
 
-echo $index > archives/index.csv

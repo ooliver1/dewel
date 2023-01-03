ea310f85c199b8a60143d22f93c3fe73096b72b3
ooliver1 <oliverwilkes2006@icloud.com>
2023-01-03 12:56:18 +0000

    Revert "refactor(rust): use cargo"
    This reverts commit 6a169bce9f75c5ded8ecbd75c4f0ad8281fe85ee.

--- packages/rust/1.66.0/build.sh
+++ packages/rust/1.66.0/build.sh
@@ -4,6 +4,3 @@ curl -OL "https://static.rust-lang.org/dist/rust-1.66.0-x86_64-unknown-linux-gnu
 tar xzvf rust-1.66.0-x86_64-unknown-linux-gnu.tar.gz
 rm rust-1.66.0-x86_64-unknown-linux-gnu.tar.gz
 rm -r rust-1.66.0-x86_64-unknown-linux-gnu/rust-docs
-./rust-1.66.0-x86_64-unknown-linux-gnu/cargo/bin/cargo init --name piston
-./rust-1.66.0-x86_64-unknown-linux-gnu/cargo/bin/cargo add anyhow futures itertools log rand serde serde_json tokio
-rm ./target/release/deps/piston*

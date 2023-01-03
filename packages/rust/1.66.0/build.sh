fa8d0072af11a4dfe854311e84aa0e2fe6852042
ooliver1 <oliverwilkes2006@icloud.com>
2023-01-03 12:56:14 +0000

    Revert "fix(rust): remove debug build"
    This reverts commit 623ee5ca6cd23eac31472febe4c8224e8f2ecb86.

--- packages/rust/1.66.0/build.sh
+++ packages/rust/1.66.0/build.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-rm -f Cargo.toml Cargo.lock
 curl -OL "https://static.rust-lang.org/dist/rust-1.66.0-x86_64-unknown-linux-gnu.tar.gz"
 tar xzvf rust-1.66.0-x86_64-unknown-linux-gnu.tar.gz
 rm rust-1.66.0-x86_64-unknown-linux-gnu.tar.gz
 rm -r rust-1.66.0-x86_64-unknown-linux-gnu/rust-docs
 ./rust-1.66.0-x86_64-unknown-linux-gnu/cargo/bin/cargo init --name piston
-./rust-1.66.0-x86_64-unknown-linux-gnu/cargo/bin/cargo add anyhow futures itertools log rand serde -F serde/derive serde_json tokio -F tokio/full
-rm ./target/debug/deps/piston*
+./rust-1.66.0-x86_64-unknown-linux-gnu/cargo/bin/cargo add anyhow futures itertools log rand serde serde_json tokio
+rm ./target/release/deps/piston*

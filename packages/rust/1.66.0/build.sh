34a1118eef643e661d35d56a303bafdf980bdff3
ooliver1 <oliverwilkes2006@icloud.com>
2023-01-03 12:56:11 +0000

    Revert "fix(rust): use rust's install.sh instead of manual"
    This reverts commit 4f5aa10f18573fe9c76447a777bf312e34b50190.

--- packages/rust/1.66.0/build.sh
+++ packages/rust/1.66.0/build.sh
@@ -4,8 +4,7 @@ rm -f Cargo.toml Cargo.lock
 curl -OL "https://static.rust-lang.org/dist/rust-1.66.0-x86_64-unknown-linux-gnu.tar.gz"
 tar xzvf rust-1.66.0-x86_64-unknown-linux-gnu.tar.gz
 rm rust-1.66.0-x86_64-unknown-linux-gnu.tar.gz
-./rust-1.66.0-x86_64-unknown-linux-gnu/install.sh
-cargo init --name piston
-cargo add anyhow base64 futures futures-util hex itertools log rand serde -F serde/derive serde_json tokio -F tokio/full
-cargo build
+rm -r rust-1.66.0-x86_64-unknown-linux-gnu/rust-docs
+./rust-1.66.0-x86_64-unknown-linux-gnu/cargo/bin/cargo init --name piston
+./rust-1.66.0-x86_64-unknown-linux-gnu/cargo/bin/cargo add anyhow futures itertools log rand serde -F serde/derive serde_json tokio -F tokio/full
 rm ./target/debug/deps/piston*
